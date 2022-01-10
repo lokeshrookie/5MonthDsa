c1fcab475d619113cd5bdf959b1ad51cc36f3dae
Lokesh <rocklinglokesh@gmail.com>
2022-01-10 22:56:51 +0530

    initial commit

--- src/chapter1/AlgorithmAnalysis/WaveArray.java
+++ src/chapter1/AlgorithmAnalysis/WaveArray.java
@@ -6,15 +6,16 @@ public class WaveArray {
     public static void main(String[] args) {
         int[] arr = {1,2,0,4,3,6,5,8,99,10};
         Arrays.sort(arr);
-        wave(arr);
+        waveArray(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void wave(int[] arr){
-        for(int i = 0; i<arr.length; i++){
-            int correct = i;
-            if(arr[i] != correct){
-                arr[i] = -1;
-            }
+    static void waveArray(int[] arr){
+        Arrays.sort(arr); // Need to sort first.
+        //skipping swapped elements with i = i+2;
+        for(int i = 0; i<arr.length-1; i+=2){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
         }
     }
 }
--- src/chapter2/Main.java
+++ src/chapter2/Main.java
@@ -1,4 +1,16 @@
 package chapter2;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Problem: Place odd elements between even elements(as a wave).
+ */
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+//        waveArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
