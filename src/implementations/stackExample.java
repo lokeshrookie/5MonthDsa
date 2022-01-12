ca9ba3269d38ef7a4f9ab1438c39c04a089e7d9d
Lokesh <rocklinglokesh@gmail.com>
2022-01-13 00:08:12 +0530

    initial commit

--- src/implementations/stackExample.java
+++ src/implementations/stackExample.java
@@ -1,16 +1,20 @@
 package implementations;
 
+import javax.swing.text.StyledEditorKit;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Stack;
 
 /**
  *
- * IMPORTANT METHODS OF STACK: Push, Pop, peek , search , empty = returns item argument.
+ * IMPORTANT METHODS OF STACK: Push, Pop, peek , search , empty, size()  = returns item argument.
  *
  * pop(),peek() - throws EmptyStack exception if the stack is empty.
  * search() - returns -1 if the element is not found.
  *
  * OTHER METHODS INCLUDE: set(index, element);
+ *                         sort(null) - null is comparator.
+ *
  *
  *
  */
@@ -21,12 +25,22 @@ public class stackExample {
         System.out.println(stack.size());
         System.out.println(stack.size());
         stack.push(5);
-        stack.push(5);
+        stack.push(10);
         stack.push(5);
         stack.push(8);
         stack.push(9);
         System.out.println(stack.toString());
         System.out.println(Arrays.toString(stack.toArray()));
+
+        stack.sort(null); // comparator is null.
+        System.out.println(stack);
+
+
+        Iterator it = stack.iterator();
+        while(it.hasNext()){
+            System.out.println(stack.pop());
+        }
+        System.out.println(stack);
 
     }
 
