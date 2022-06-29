835b82c4557f9bd4ae81e503f275a95d17cdf706
Lokesh <rocklinglokesh@gmail.com>
2022-06-29 20:02:51 +0530

    2/75

---
+++ src/LeetCode/L75/FindPivotIndex.java
@@ -0,0 +1,30 @@
+package LeetCode.L75;
+
+public class FindPivotIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(arr));
+    }
+
+    public static  int pivotIndex(int[] nums) {
+        // First find the total sum
+        int total_sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total_sum += nums[i];
+        }
+
+        // find the pivot index.
+        // we have to compare right sum and left sum
+        // right sum = totalsum - nums[i] - leftsum
+        // totalsum - nums[i] - leftsum == leftsum;
+        int left_sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0)
+                left_sum += nums[i - 1]; // we skipped 0. so we have to add previous element. and leave the last element.
+            if (total_sum - left_sum - nums[i] == left_sum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
