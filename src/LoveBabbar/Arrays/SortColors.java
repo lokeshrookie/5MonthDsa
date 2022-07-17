d14bba1c6536ba11110c01a48d5732bbec3fe8dd
Lokesh <rocklinglokesh@gmail.com>
2022-07-17 16:24:22 +0530

    babbar 4/450

--- src/LoveBabbar/Arrays/SortColors.java
+++ src/LoveBabbar/Arrays/SortColors.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SortColors {
     public static void main(String[] args) {
         int[] arr ={2,1,1,1,2,0,0,0,0,2,1,1,2,1,2,1,2,0,};
-        System.out.println(Arrays.toString(sortColors(arr)));
+        System.out.println(Arrays.toString(colors(arr)));
     }
 
     /** BruteForce */
@@ -39,5 +39,38 @@ public class SortColors {
             }
         }
         return arr;
+    }
+
+    /** OPTIMISED : DUTCH NATIONAL FLAG ALGORITHM */
+    /*
+    1. while iterating, if you find that the element is 0, swap that element with element at start, increment start and index.
+    2. if element is 2, swap with last, decrement last, increment index.
+    3. if element is 1, just increase index. don't need to do anything.
+
+        In this way, we're just swapping all the 0's with 2's and vice-versa. All the 1's will remain in the middle.
+     */
+    static int[] colors(int[] arr){
+        if(arr.length <=1 ) return arr;
+        int index = 0;
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end && index <= end){
+            if(arr[index] == 0){
+                arr[index] = arr[start];
+                arr[start] = 0; // same as element at index.
+                start++;
+                index++;
+            }
+            else if(arr[index] == 2){
+                arr[index] = arr[end];
+                arr[end] = 2; // same as element at index.
+                index++;
+                end--;
+            }
+            else{
+                index++;
+            }
+        }
+        return  arr;
     }
 }
