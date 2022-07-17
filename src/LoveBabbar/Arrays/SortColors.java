35d78d6407c38dbbff696f5586ea35d94a420d94
Lokesh <rocklinglokesh@gmail.com>
2022-07-17 16:08:22 +0530

    babbar 4/450

--- src/LoveBabbar/Arrays/SortColors.java
+++ src/LoveBabbar/Arrays/SortColors.java
@@ -1,8 +1,43 @@
 package LoveBabbar.Arrays;
 
+import java.util.Arrays;
+
 public class SortColors {
     public static void main(String[] args) {
-
+        int[] arr ={2,1,1,1,2,0,0,0,0,2,1,1,2,1,2,1,2,0,};
+        System.out.println(Arrays.toString(sortColors(arr)));
     }
 
+    /** BruteForce */
+    static int[] sortColors(int[] arr){
+        /*
+        BruteForce Algorithm : Counting Sort
+         */
+        int zero = 0, one = 0, two = 0;
+        for (int j : arr) {
+            if (j == 0) {
+                zero += 1;
+            } else if (j == 1) {
+                one += 1;
+            } else {
+                two += 1;
+            }
+        }
+        int index = 0;
+        while(zero > 0 || one >0 || two >0){
+            if(zero > 0) {
+                arr[index++] = 0;
+                zero--;
+            }
+            else if(one > 0) {
+                arr[index++] = 1;
+                one--;
+            }
+            else {
+                arr[index++] = 2;
+                two--;
+            }
+        }
+        return arr;
+    }
 }
