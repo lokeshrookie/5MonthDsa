6cf614cf06c2ce1b5ff650b40acb81b91451a39d
Lokesh <rocklinglokesh@gmail.com>
2022-07-20 19:17:57 +0530

    babbar 7/450

---
+++ src/LoveBabbar/Arrays/BestTImeToBuyAndSellStocks.java
@@ -0,0 +1,17 @@
+package LoveBabbar.Arrays;
+
+public class BestTImeToBuyAndSellStocks {
+    public static void main(String[] args) {
+        int[] arr = { 7, 1, 5, 6, 4 };
+        System.out.println(stocks(arr));
+    }
+    static int stocks(int[] arr){
+        int minPrice = arr[0];
+        int maxProfit = 0;
+        for (int j : arr) {
+            if (j < minPrice) minPrice = j;
+            if (j - minPrice > maxProfit) maxProfit = j - minPrice;
+        }
+        return maxProfit;
+    }
+}
